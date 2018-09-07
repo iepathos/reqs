91db8623faca377c280f0de086a67754e7610cf8
Glen Baker <glen@headspin.io>
2018-09-06 17:26:51 -0700

    cleanup main logic for multiple package management tools

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # sysreqs
 
 Abstract cross-platform package install tool.  Takes a requirements file, like a pip requirements.txt with package names each on a new line and it tries to install the packages listed in it using either apt, brew, or dnf depending on the operating system.
+
+
+Usage:
+
+```
+sysreqs < sys-requirements.txt
+
+```
--- sysreqs.go
+++ sysreqs.go
@@ -29,27 +29,39 @@ func installHomebrew() {
 }
 
 func main() {
+    // if arg -d then check the directory for <sys>-requirements.txt files and use them
+    // if arg -f then use the specified file for requirements
+    // if no args use stdin
+
     reader := bufio.NewReader(os.Stdin)
     text, _ := reader.ReadString('\n')
     text = strings.Replace(text, "\n", " ", -1)
 
+    var packageTool string
+    autoYes := ""
     if runtime.GOOS == "linux" {
-        log.Info("Linux system detected, installing system requirements with apt")
-        cmd := exec.Command("/bin/sh", "-c", "sudo apt install -y"+text)
-        err := cmd.Run()
-        if err != nil {
-            log.Fatal(err)
+        log.Info("Linux system detected")
+        if isCommandAvailable("apt") {
+            packageTool = "apt"
+        } else if isCommandAvailable("dnf") {
+            packageTool = "dnf"
+        } else if isCommandAvailable("yum") {
+            packageTool = "yum"
         }
+        autoYes = "-y "
     } else if runtime.GOOS == "darwin" {
         log.Info("Darwin system detected")
         if !isCommandAvailable("brew") {
             installHomebrew()
         }
-        log.Info("Installing system requirements with brew")
-        cmd := exec.Command("/bin/sh", "-c", "brew install "+text)
-        err := cmd.Run()
-        if err != nil {
-            log.Fatal(err)
-        }
+        packageTool = "brew"
+    } else if runtime.GOOS == "windows" {
+        log.Fatal("Windows system detected, abandon all hope")
+    }
+    log.Info("Installing system requirements with " + packageTool)
+    cmd := exec.Command("/bin/sh", "-c", "brew install "+autoYes+text)
+    err := cmd.Run()
+    if err != nil {
+        log.Fatal(err)
     }
 }
