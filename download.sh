5034f0ac967e2b9a5f0893e8965194de0b98f57a
Glen Baker <glen@headspin.io>
2018-09-09 11:57:23 -0700

    refactor download script

--- download.sh
+++ download.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # downloads reqs from github release binary
+version="0.2.9"
 arch="$(uname)"
+
 if [[ "$arch" == "Darwin" ]]; then
-	curl -sL https://github.com/iepathos/reqs/releases/download/v0.2.8/reqs_0.2.8_Darwin_x86_64.tar.gz > reqs.tar.gz
+	url="https://github.com/iepathos/reqs/releases/download/v${version}/reqs}${version}_Darwin_x86_64.tar.gz"
 else
-	curl -sL https://github.com/iepathos/reqs/releases/download/v0.2.8/reqs_0.2.8_Linux_x86_64.tar.gz > reqs.tar.gz
+	url="https://github.com/iepathos/reqs/releases/download/v${version}/reqs_${version}_Linux_x86_64.tar.gz"
 fi
+echo $url
+curl -sL $url > reqs.tar.gz
 tar -xzf reqs.tar.gz
 rm reqs.tar.gz
