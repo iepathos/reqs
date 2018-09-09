5016ab5ee02d499e617553e947a3a899cda32dc8
Glen Baker <glen@headspin.io>
2018-09-09 00:10:21 -0700

    update download to v0.2.6

--- download.sh
+++ download.sh
@@ -2,9 +2,9 @@
 # downloads reqs from github release binary
 arch="$(uname)"
 if [[ "$arch" == "Darwin" ]]; then
-	curl -sL https://github.com/iepathos/reqs/releases/download/v0.2.4/reqs_0.2.4_Darwin_x86_64.tar.gz > reqs.tar.gz
+	curl -sL https://github.com/iepathos/reqs/releases/download/v0.2.6/reqs_0.6.4_Darwin_x86_64.tar.gz > reqs.tar.gz
 else
-	curl -sL https://github.com/iepathos/reqs/releases/download/v0.2.4/reqs_0.2.4_Linux_x86_64.tar.gz > reqs.tar.gz
+	curl -sL https://github.com/iepathos/reqs/releases/download/v0.2.6/reqs_0.6.4_Linux_x86_64.tar.gz > reqs.tar.gz
 fi
 tar -xzf reqs.tar.gz
 rm reqs.tar.gz
