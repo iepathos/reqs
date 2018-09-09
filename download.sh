0739c7ba429120bf6b786e57d955dc07e433d94e
Glen Baker <glen@headspin.io>
2018-09-09 12:11:51 -0700

    improve download script and make release update it automatically

--- download.sh
+++ download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # downloads reqs from github release binary
-version="0.2.9"
+version="v0.2.9"
 arch="$(uname)"
 
 if [[ "$arch" == "Darwin" ]]; then
-	url="https://github.com/iepathos/reqs/releases/download/v${version}/reqs}${version}_Darwin_x86_64.tar.gz"
+	url="https://github.com/iepathos/reqs/releases/download/v${version//v}/reqs}${version//v}_Darwin_x86_64.tar.gz"
 else
-	url="https://github.com/iepathos/reqs/releases/download/v${version}/reqs_${version}_Linux_x86_64.tar.gz"
+	url="https://github.com/iepathos/reqs/releases/download/v${version//v}/reqs_${version//v}_Linux_x86_64.tar.gz"
 fi
 echo $url
 curl -sL $url > reqs.tar.gz
--- release.sh
+++ release.sh
@@ -9,3 +9,11 @@ fi
 
 go get -u github.com/goreleaser/goreleaser
 goreleaser $distarg
+
+# update download version to latest git tag
+previoustag="$(git tag | tail -n 2 | head -n 1)"
+latesttag="$(git tag | tail -n 1)"
+sed "s/$previoustag/$latesttag/" < download.sh > download.sh
+git add download.sh
+git commit -m "updated download.sh from $previoustag to $latesttag"
+git push origin master
