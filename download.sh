d5d170e8d7c81b49c6f61364e534d612c0e63c2f
Glen Baker <glen@headspin.io>
2018-09-09 12:21:03 -0700

    cleanup url

--- download.sh
+++ download.sh
@@ -4,9 +4,9 @@ version="v0.3.0"
 arch="$(uname)"
 
 if [[ "$arch" == "Darwin" ]]; then
-	url="https://github.com/iepathos/reqs/releases/download/v${version//v}/reqs}${version//v}_Darwin_x86_64.tar.gz"
+	url="https://github.com/iepathos/reqs/releases/download/${version}/reqs}${version//v}_Darwin_x86_64.tar.gz"
 else
-	url="https://github.com/iepathos/reqs/releases/download/v${version//v}/reqs_${version//v}_Linux_x86_64.tar.gz"
+	url="https://github.com/iepathos/reqs/releases/download/${version}/reqs_${version//v}_Linux_x86_64.tar.gz"
 fi
 echo $url
 curl -sL $url > reqs.tar.gz
