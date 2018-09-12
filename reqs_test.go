03ded82ca2f05cb36b15bdb790afd3775fa14c9a
Glen Baker <glen@headspin.io>
2018-09-11 19:27:11 -0700

    cleanup

--- reqs_test.go
+++ reqs_test.go
@@ -8,7 +8,7 @@ import (
 func testReqsOn(arch, reqsArgsStr string) error {
 	vm := VagrantSystem{
 		Arch:   arch,
-		Status: "down",
+		Status: "",
 	}
 	vm.Down()
 	err := vm.Run("reqs " + reqsArgsStr)
@@ -19,14 +19,26 @@ func testReqsOn(arch, reqsArgsStr string) error {
 func TestReqsUbuntu(t *testing.T) {
 	err := testReqsOn("ubuntu", "-r")
 	assert.Nil(t, err)
+
+	// test with update and upgrade
+	err = testReqsOn("ubuntu", "-r -up")
+	assert.Nil(t, err)
 }
 
 func TestReqsFedora(t *testing.T) {
 	err := testReqsOn("fedora", "-r")
 	assert.Nil(t, err)
+
+	// test with update and upgrade
+	err = testReqsOn("fedora", "-r -up")
+	assert.Nil(t, err)
 }
 
 func TestReqsOsx(t *testing.T) {
 	err := testReqsOn("osx", "-r")
+	assert.Nil(t, err)
+
+	// test with update and upgrade
+	err = testReqsOn("osx", "-r -up")
 	assert.Nil(t, err)
 }
